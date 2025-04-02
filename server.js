89173f32798f49bde0c8037f2f667f148de41a56
Eduarda Alves <alvesneivam1203@gmail.com>
2025-04-02 11:17:47 -0300

    Update v02

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+const user = []
+
+//Criar dados / usuarios / fazer requisição POST
+app.post('Users', (req, res) =>{
+    users.push(req.body);
+    res.status(201).json(users);
+})
+
+//Mostrar / dados / usuarios / fazer requisição GET
 app.get('/users', (req, res) => {
     //res.status(200).json(users);
-    res.send('ok, I am here.');
+    res.status(200).json(users);
 });
 
 //ip 127.0.0.1:8080*/
